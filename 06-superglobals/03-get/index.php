ca62562cf4ea75208aef3d25fb6e9891e036d89b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-10-21 09:36:08 +0300

    Learnt about Get Superglobal

--- 06-superglobals/03-get/index.php
+++ 06-superglobals/03-get/index.php
@@ -1,3 +1,7 @@
 <?php
+
+
+//echo isset($_GET['name']) ? $_GET['name'] : "";
+echo htmlspecialchars($_GET['name'] ?? '');
 // Add a query parameter to the URL in the browser's address bar:
 // http://localhost:8000/?name=John
