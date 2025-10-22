4720a1a884332b8e34587f17869b563a75114f06
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-10-22 23:05:01 +0300

    Learnt how to fetch records from msql database in php

--- 07-database/08-select-records/index.php
+++ 07-database/08-select-records/index.php
@@ -1,3 +1,20 @@
+<?php
+
+require 'database.php';
+
+//prepare select statement
+$stmt = $pdo->prepare("SELECT * FROM posts");
+
+//execute the statement
+$stmt->execute();
+
+//Fetch the results
+$posts = $stmt-> fetchAll();
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,22 +32,21 @@
     </div>
   </header>
   <div class="container mx-auto p-4 mt-4">
+      <?php foreach ($posts as $post) : ?>
     <div class="md my-4">
       <div class="rounded-lg shadow-md">
         <div class="p-4">
-          <h2 class="text-xl font-semibold">Post One</h2>
-          <p class="text-gray-700 text-lg mt-2">This is post one</p>
+          <h2 class="text-xl font-semibold">
+              <a href="post.php?id=<?=$post['id']?>">
+                  <?=$post['title']?>
+              </a>
+
+            </h2>
+          <p class="text-gray-700 text-lg mt-2"><?=$post['body']?></p>
         </div>
       </div>
     </div>
-    <div class="md my-4">
-      <div class="rounded-lg shadow-md">
-        <div class="p-4">
-          <h2 class="text-xl font-semibold">Post Two</h2>
-          <p class="text-gray-700 text-lg mt-2">This is post two</p>
-        </div>
-      </div>
-    </div>
+   <?php endforeach; ?>
   </div>
 </body>
 
